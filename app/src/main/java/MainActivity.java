7829509cc0e4ee9fedf06bf7b0681b4b890919cc
Jack Williams <jack@stcwilliams.com>
2018-04-30 22:45:59 -0500

    Finished project

--- app/src/main/java/MainActivity.java
+++ app/src/main/java/MainActivity.java
@@ -6,31 +6,26 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
-import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonArray;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.util.ArrayList;
 
 /**
  * Main Activity class for MP7.
  */
-public class MainActivity extends AppCompatActivity implements OnItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
     /**
      * Debugging tag.
      */
@@ -52,93 +47,35 @@ public class MainActivity extends AppCompatActivity implements OnItemSelectedLis
      */
     private ArrayList<String> coins = new ArrayList<>();
     /**
-     *Coin currently selected on the spinner
+     *Coin currently selected on the .
      */
-    private String selectedCoin;
+    private String selectedCoin = "Bitcoin";
 
     /**
      * On create method.
-     * @param savedInstanceState daved instance state
+     * @param savedInstanceState saved instance state
      */
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         requestQueue = Volley.newRequestQueue(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // start building the textview
-        atextviewresult = findViewById(R.id.coinName);
-        btextviewresult = findViewById(R.id.price);
-
-        ctextviewresult = findViewById(R.id.text2);
-        dtextviewresult = findViewById(R.id.text3);
-        etextviewresult = findViewById(R.id.text4);
-        ftextviewresult = findViewById(R.id.text5);
-        gtextviewresult = findViewById(R.id.text6);
-        htextviewresult = findViewById(R.id.text7);
-       myQueue = Volley.newRequestQueue(this);
-
         startAPICall(requestQueue);
         Log.d(TAG, "Starting API Call ");
-        Spinner spinner = findViewById(R.id.spinner);
-        spinner.setOnItemSelectedListener(this);
-        // Creating adapter for spinner
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, coins);
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        // attaching data adapter to spinner
-        spinner.setAdapter(dataAdapter);
-
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Log.d(TAG, "Refresh button clicked");
+                startAPICall(requestQueue);
+            }
+        });
     }
-
     /**
      * Starts the API Call to get information.
      * @param requestQueue1 request queue for project
      */
-    /** I am not entirely sure what I am doing here, I just basically fowllow this video: https://www.youtube.com/watch?v=y2xtLqP8dSQ, very useful tho. I
-     * redo some part of it because I don't think your stringrequest would work. This part is just get the data from json and put them into texts.
-     *
-     * it can either be run here or the one below.
-     * @param requestQueue1 request queue for Json
-     */
     private void startAPICall(final RequestQueue requestQueue1) {
-
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, TARGET_URL, null, new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-                JsonParser parser = new JsonParser();
-                JsonArray jsonArray = parser.parse(response).getAsJsonArray();
-                try {
-
-                    String priceBtc = response.getString("price_btc");
-                    String name = response.getString("name");
-                    String priceUsd = response.getString("price_usd");
-                    String change24h = response.getString("percent_change_24h");
-                    String marketCap = response.getString("market_cap_usd");
-                    String change7d = response.getString("percent_change_7d");
-                    String change1h = response.getString("percent_change_1h");
-                    String totalSupply = response.getString("total_supply");
-
-                    atextviewresult.append(name);
-                    btextviewresult.append(priceUsd);
-                    ctextviewresult.append(change24h);
-                    dtextviewresult.append(priceBtc);
-                    etextviewresult.append(marketCap);
-                    ftextviewresult.append(change1h);
-                    gtextviewresult.append(change7d);
-                    htextviewresult.append(totalSupply);
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                error.printStackTrace();
-            }
-    });
-        myQueue.add(request);
-
         StringRequest arrayRequest = new StringRequest(
                 Request.Method.GET,
                 TARGET_URL,
@@ -146,7 +83,7 @@ public class MainActivity extends AppCompatActivity implements OnItemSelectedLis
                     @Override
                     public void onResponse(final String response) {
 
-                        Log.d(TAG, "Got response:");
+                        Log.d(TAG, "Got response.");
                         Log.d(TAG, response);
                         finishAPICall(response);
                     }
@@ -174,86 +111,62 @@ public class MainActivity extends AppCompatActivity implements OnItemSelectedLis
         for (JsonElement j : jsonArray) {
             coins.add(j.getAsJsonObject().get("name").getAsString());
         }
-        //NEEDED: Fill TextView boxes here.
-        /** something is wrong here I dont know how to fix this.
-         *
-         * */
-try {
+        Spinner spinner = findViewById(R.id.spinner);
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, R.layout.spinner_dropdown_item, coins);
+        dataAdapter.setDropDownViewResource(R.layout.spinner_item);
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(final AdapterView<?> parent, final View view,
+                                       final int position, final long id) {
+                selectedCoin = parent.getItemAtPosition(position).toString();
+                Log.d(TAG, "Selected coin: " + selectedCoin);
+            }
 
-            JsonObject jsonObject = data.getAsJsonObject();
-    String priceBtc = jsonObject.get("price_btc").getAsString();
-    String name = jsonObject.get("name").getAsString();
-    String priceUsd = jsonObject.get("price_usd").getAsString();
-    String change24h = jsonObject.get("percent_change_24h").getAsString();
-    String marketCap = jsonObject.get("market_cap_usd").getAsString();
-    String change7d = jsonObject.get("percent_change_7d").getAsString();
-    String change1h = jsonObject.get("percent_change_1h").getAsString();
-    String totalSupply = jsonObject.get("total_supply").getAsString();
+            @Override
+            public void onNothingSelected(final AdapterView<?> parent) {
 
-    atextviewresult.setText(name);
-    btextviewresult.setText(priceUsd);
-    ctextviewresult.setText(change24h);
-    dtextviewresult.setText(priceBtc);
-    etextviewresult.setText(marketCap);
-    ftextviewresult.setText(change1h);
-    gtextviewresult.setText(change7d);
-    htextviewresult.setText(totalSupply);
-    Log.d(TAG, response.toString());
-} catch (Exception ignored) {
-}
-/** I think this part does not work, though keep it just in case
-for (JsonElement i: jsonArray) {
-
-    String priceBtc = i.getAsJsonObject().get("price_btc").getAsString();
-    String name = i.getAsJsonObject().get("name").getAsString();
-    String priceUsd = i.getAsJsonObject().get("price_usd").getAsString();
-    String change24h = i.getAsJsonObject().get("percent_change_24h").getAsString();
-    String marketCap = i.getAsJsonObject().get("market_cap_usd").getAsString();
-    String change7d = i.getAsJsonObject().get("percent_change_7d").getAsString();
-    String change1h = i.getAsJsonObject().get("percent_change_1h").getAsString();
-    String totalSupply = i.getAsJsonObject().get("total_supply").getAsString();
-    atextviewresult.append(name);
-    btextviewresult.append(priceUsd);
-    ctextviewresult.append(change24h);
-    dtextviewresult.append(priceBtc);
-    etextviewresult.append(marketCap);
-    ftextviewresult.append(change1h);
-    gtextviewresult.append(change7d);
-    htextviewresult.append(totalSupply);
-}
- */
-        JsonObject coinObject;
+            }
+        });
+        JsonObject coinObject = null;
         for (JsonElement j : jsonArray) {
             if (j.getAsJsonObject().get("name").getAsString().equals(selectedCoin)) {
                 coinObject = j.getAsJsonObject();
                 break;
             }
         }
+        try {
+            String priceBtc1 = coinObject.get("price_btc").getAsString();
+            String name1 = coinObject.get("name").getAsString();
+            String priceUsd1 = coinObject.get("price_usd").getAsString();
+            String change24h1 = coinObject.get("percent_change_24h").getAsString();
+            String marketCap1 = coinObject.get("market_cap_usd").getAsString();
+            String change7d1 = coinObject.get("percent_change_7d").getAsString();
+            String change1h1 = coinObject.get("percent_change_1h").getAsString();
+            String totalSupply1 = coinObject.get("total_supply").getAsString();
 
-        Log.d(TAG, coins.toString());
-    }
+            TextView coinName = findViewById(R.id.coinName);
+            TextView price = findViewById(R.id.price);
+            TextView change24h = findViewById(R.id.chnage24h);
+            TextView priceBTC  = findViewById(R.id.priceBTC);
+            TextView marketCap = findViewById(R.id.marketCap);
+            TextView change7d = findViewById(R.id.change7d);
+            TextView totalSupply = findViewById(R.id.totalSupply);
+            TextView change1h = findViewById(R.id.change1h);
 
-// declaring all the variable that I am using to represent the texts
-
-    private TextView atextviewresult, btextviewresult, ctextviewresult, dtextviewresult,
-    etextviewresult, ftextviewresult,gtextviewresult, htextviewresult;
-    private RequestQueue myQueue;
-
-    /**
-     * Sets the coin selected in the spinner to local variable.
-     * @param parent idk
-     * @param view idk
-     * @param i pos of item
-     * @param l idk
-     */
-    @Override
-    public void onItemSelected(final AdapterView<?> parent, final View view, final int i, final long l) {
-        selectedCoin = (String) parent.getItemAtPosition(i);
-        startAPICall(requestQueue);
-    }
-
-    @Override
-    public void onNothingSelected(final AdapterView<?> adapterView) {
-
+            coinName.setText(name1);
+            price.setText(priceUsd1);
+            change24h.setText(change24h1);
+            priceBTC.setText(priceBtc1);
+            marketCap.setText(marketCap1);
+            change1h.setText(change1h1);
+            change7d.setText(change7d1);
+            totalSupply.setText(totalSupply1);
+        } catch (Exception e) {
+            Log.d(TAG, "Couldn't get coin details: ");
+            Log.d(TAG, e.toString());
+        }
     }
 }
